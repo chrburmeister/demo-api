26c6693a6c334fd026b05396098e9ce6c314b1d6
Christoph Burmeister <christoph.burmeister@outlook.de>
2025-05-23 07:01:14 +0200

    feat(demo-api-py): :sparkles:  adding endpoints to return passed headers and body

--- python/server.py
+++ python/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
@@ -9,3 +9,15 @@ async def root():
 @app.get("/status")
 async def get_status():
     return {"status": "healthy"}
+
+@app.get("/headers")
+async def get_headers(request: Request):
+    print(request.headers)
+    return {"headers": dict(request.headers)}
+
+@app.post("/body")
+async def get_headers(request: Request):
+    body = await request.body()
+    body_str = body.decode('utf-8') if body else ""
+    print(body_str)
+    return {"body": body_str}
