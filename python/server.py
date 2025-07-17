37a01cfb153f70e6566fbedc74ea4989ca5fd6ae
Christoph Burmeister <christoph.burmeister@outlook.de>
2025-07-17 10:02:30 +0200

    feat(demo-api-py): :sparkles:  adding location endpoint

--- python/server.py
+++ python/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+import os
 
 app = FastAPI()
 
@@ -10,13 +11,18 @@ async def root():
 async def get_status():
     return {"status": "healthy"}
 
+@app.get("/location")
+async def get_location():
+    location = os.getenv('LOCATION', "not_set")
+    return {"location": location}
+
 @app.get("/headers")
 async def get_headers(request: Request):
     print(request.headers)
     return {"headers": dict(request.headers)}
 
 @app.post("/body")
-async def get_headers(request: Request):
+async def get_body(request: Request):
     body = await request.body()
     body_str = body.decode('utf-8') if body else ""
     print(body_str)
